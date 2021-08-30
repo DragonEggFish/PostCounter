1197eeeb6000b0d5c5b26a581c2c46c91afe6aa6
DragonEggFish <88898601+DragonEggFish@users.noreply.github.com>
2021-08-30 17:27:04 +0200

    Create post count.py
    easy

---
+++ post count.py
@@ -0,0 +1,4 @@
+import requests
+user = input('username: ')
+postcount = requests.get('https://scratchdb.lefty.one/v3/forum/user/info/'+user).json()['counts']['total']['count']
+print(f'{user} has {postcount} posts.')
