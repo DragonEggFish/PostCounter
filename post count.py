228750d17854ce4bd7678571acbee42e504bebeb
DragonEggFish <88898601+DragonEggFish@users.noreply.github.com>
2021-09-01 18:55:28 +0200

    Update post count.py

--- post count.py
+++ post count.py
@@ -1,4 +1,4 @@
-import requests
+import requests # imports ig
 user = input('username: ')
 postcount = requests.get('https://scratchdb.lefty.one/v3/forum/user/info/'+user).json()['counts']['total']['count']
 print(f'{user} has {postcount} posts.')
